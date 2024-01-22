6359275034a71138d6e954b539d2c4bde01306c3
Intisarelkharraz <elkharrazintisar2002@gmail.com>
2024-01-22 13:44:04 +0100

    DEV-intisar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardRoutingModule } from './dashboard/dashboard-routing.module';
 import { FormateurRoutingModule } from './dashboard/formateur/formateur-routing.module';
-
+import { WelcomeRoutingModule } from './welcome/welcome-routing.module';
+import { LoginRoutingModule } from './login/login-routing.module';
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes),
-    DashboardRoutingModule,FormateurRoutingModule
+    DashboardRoutingModule,FormateurRoutingModule,WelcomeRoutingModule,LoginRoutingModule
   ],
   exports: [RouterModule]
 })
